02c5a0525f8b634fda5811746f30ba4cc24d9b75
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-26 19:21:26 +0300

    refactor: change code indentation size to 2 spaces

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
