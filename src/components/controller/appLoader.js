57fab80f72a7ba5d3635a83233d32f6554c39681
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-26 21:13:47 +0300

    feat: use external webpack bundle settings for TS

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
-      apiKey: '', // получите свой ключ https://newsapi.org/
+    super('https://rss-news-api.onrender.com/', {
+      apiKey: '52ec34c0d4494591ae284ed09034b76c', // получите свой ключ https://newsapi.org/
     });
   }
 }
