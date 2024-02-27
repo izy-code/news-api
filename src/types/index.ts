1247dd6d54d1bf3d3513526cf6367b51062a2356
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 00:38:40 +0300

    feat: add interfaces and status enum

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+export enum Status {
+  OK = 'ok',
+  ERROR = 'error',
+}
+
+export interface Article {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
+
+export interface Source {
+  id: string | null;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
+
+interface Response {
+  status: `${Status}`;
+  code?: string;
+  message?: string;
+}
+
+export interface NewsResponse extends Response {
+  articles: Article[];
+}
+
+export interface SourcesResponse extends Response {
+  sources: Source[];
+}
