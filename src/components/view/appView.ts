3f7942dad3d9ad2a165da88f19a3d48106569858
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 00:41:10 +0300

    refactor: replace JS appView module code with TS

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,19 +1,24 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import type { NewsResponse, SourcesResponse } from '@/types';
 
 export class AppView {
+  private news;
+  private sources;
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
+  public drawNews(data: NewsResponse): void {
     const values = data?.articles ? data?.articles : [];
+
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  public drawSources(data: SourcesResponse): void {
     const values = data?.sources ? data?.sources : [];
+
     this.sources.draw(values);
   }
 }
