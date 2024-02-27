d7993cb17ed8e0bbea73e1099f547dbb2351f384
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-27 18:38:52 +0300

    fix: return initial process properties for loader path

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://rss-news-api.onrender.com/', {
-      apiKey: '52ec34c0d4494591ae284ed09034b76c', // получите свой ключ https://newsapi.org/
+    super(process.env.API_URL, {
+      apiKey: process.env.API_KEY,
     });
   }
 }
