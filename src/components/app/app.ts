34411df38b59ed0fe5001c8f6a2d4b17d391aa60
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 18:36:14 +0300

    refactor: replace app module JS code with TS

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,15 +1,18 @@
+import { queryElement } from '@/utils';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+  private controller: AppController;
+  private view: AppView;
+
   constructor() {
     this.controller = new AppController();
     this.view = new AppView();
   }
 
-  start() {
-    document
-      .querySelector('.sources')
+  public start(): void {
+    queryElement(document, '.sources', HTMLDivElement)
       .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
     this.controller.getSources((data) => this.view.drawSources(data));
   }
