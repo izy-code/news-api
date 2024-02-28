b1c86e09f14064cec6f4a3c2d0384a30fc144955
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 13:43:20 +0300

    feat: add assertElementType and replace findElement with queryElement

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,34 @@
+type ConstructorOf<T> = { new (...args: unknown[]): T; prototype: T };
+
 export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
   if (value === undefined || value === null) {
     throw new Error(`Value not defined`);
   }
+
+  if (value === null) {
+    throw new Error(`Value is null`);
+  }
 }
 
-export function findElement<T extends Element>(container: Element | Document | DocumentFragment, selector: string): T {
-  const element = container.querySelector<T>(selector);
+export function assertElementType<T>(value: T, expectedType: ConstructorOf<T>): asserts value is NonNullable<T> {
+  assertIsDefined(value);
 
-  assertIsDefined(element);
-
-  return element;
+  if (!(value instanceof expectedType)) {
+    throw new TypeError(`Wrong node type`);
+  }
 }
+
+export const queryElement = <T extends Element>(
+  container: Element | Document | DocumentFragment,
+  selector: string,
+  expectedType: ConstructorOf<T>,
+): T => {
+  const queriedElement = container.querySelector<T>(selector);
+
+  assertElementType(queriedElement, expectedType);
+
+  return queriedElement;
+};
 
 export function assertIsQuerySelectable(node: Node): asserts node is Document | DocumentFragment | Element {
   if (
