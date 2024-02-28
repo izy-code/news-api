89a91e65ca29c50daf76816bdaca28e8e79f1517
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 18:36:59 +0300

    fix: add type narrowing from union type

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,25 +1,32 @@
+import type { SourcesResponseObject, NewsResponseObject } from '@/types';
 import News from './news/news';
 import Sources from './sources/sources';
-import type { NewsResponseObject, SourcesResponseObject } from '@/types';
 
 export class AppView {
   private news;
   private sources;
+
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  public drawNews(data: NewsResponseObject): void {
-    const values = data?.articles ? data?.articles : [];
-
-    this.news.draw(values);
+  public drawNews(data: SourcesResponseObject | NewsResponseObject): void {
+    if ('articles' in data) {
+      this.news.draw(data.articles);
+      return;
+    } else {
+      this.news.draw([]);
+    }
   }
 
-  public drawSources(data: SourcesResponseObject): void {
-    const values = data?.sources ? data?.sources : [];
-
-    this.sources.draw(values);
+  public drawSources(data: SourcesResponseObject | NewsResponseObject): void {
+    if ('sources' in data) {
+      this.sources.draw(data.sources);
+      return;
+    } else {
+      this.sources.draw([]);
+    }
   }
 }
 
