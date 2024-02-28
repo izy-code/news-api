1d324895ce548e92118b107cbbff28a77d54b2fa
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 16:24:10 +0300

    feat: add API_URL and API_KEY props to NodeJS.ProcessEnv interface

---
+++ src/process-env.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      API_URL: string;
+      API_KEY: string;
+
+      [key: string]: string | undefined;
+    }
+  }
+}
+
+export {}
