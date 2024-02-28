c08bfb4b75b108942f76fafef266d2538007aa66
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 14:04:42 +0300

    refactor: replace sources module JS code with TS

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,25 @@
+import type { Source } from '@/types';
+import { assertObjectType, queryElement } from '@/utils';
+import './sources.css';
+
+class Sources {
+  public draw(data: Source[]): void {
+    const fragment = document.createDocumentFragment();
+    const sourceItemTemp = queryElement(document, '#sourceItemTemp', HTMLTemplateElement);
+
+    data.forEach((item) => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true);
+
+      assertObjectType(sourceClone, HTMLElement);
+
+      queryElement(sourceClone, '.source__item-name', HTMLElement).textContent = item.name;
+      queryElement(sourceClone, '.source__item', HTMLElement).setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    queryElement(document, '.sources', HTMLElement).append(fragment);
+  }
+}
+
+export default Sources;
