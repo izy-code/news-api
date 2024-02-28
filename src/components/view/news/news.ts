c63b09d09b8e77291db92021753f48c6f2090380
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-28 13:44:42 +0300

    fix: use queryElement instead of findElement

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,13 +1,13 @@
 import type { Article } from '@/types';
+import { assertIsQuerySelectable, queryElement } from '@/utils';
 import './news.css';
-import { assertIsQuerySelectable, findElement } from '@/utils';
 
 class News {
   public draw(data: Article[]): void {
     const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
     const fragment = document.createDocumentFragment();
-    const newsItemTemp = findElement<HTMLTemplateElement>(document, '#newsItemTemp');
+    const newsItemTemp = queryElement(document, '#newsItemTemp', HTMLTemplateElement);
 
     news.forEach((item, idx) => {
       const newsClone = newsItemTemp.content.cloneNode(true);
@@ -15,29 +15,29 @@ class News {
       assertIsQuerySelectable(newsClone);
 
       if (idx % 2) {
-        findElement<HTMLElement>(newsClone, '.news__item').classList.add('alt');
+        queryElement(newsClone, '.news__item', HTMLElement).classList.add('alt');
       }
 
-      findElement<HTMLElement>(newsClone, '.news__meta-photo').style.backgroundImage = `url(${
+      queryElement(newsClone, '.news__meta-photo', HTMLElement).style.backgroundImage = `url(${
         item.urlToImage || 'img/news_placeholder.jpg'
       })`;
-      findElement<HTMLElement>(newsClone, '.news__meta-author').textContent = item.author || item.source.name;
-      findElement<HTMLElement>(newsClone, '.news__meta-date').textContent = item.publishedAt
+      queryElement(newsClone, '.news__meta-author', HTMLElement).textContent = item.author || item.source.name;
+      queryElement(newsClone, '.news__meta-date', HTMLElement).textContent = item.publishedAt
         .slice(0, 10)
         .split('-')
         .reverse()
         .join('-');
 
-      findElement<HTMLElement>(newsClone, '.news__description-title').textContent = item.title;
-      findElement<HTMLElement>(newsClone, '.news__description-source').textContent = item.source.name;
-      findElement<HTMLElement>(newsClone, '.news__description-content').textContent = item.description;
-      findElement<HTMLElement>(newsClone, '.news__read-more a').setAttribute('href', item.url);
+      queryElement(newsClone, '.news__description-title', HTMLElement).textContent = item.title;
+      queryElement(newsClone, '.news__description-source', HTMLElement).textContent = item.source.name;
+      queryElement(newsClone, '.news__description-content', HTMLElement).textContent = item.description;
+      queryElement(newsClone, '.news__read-more a', HTMLElement).setAttribute('href', item.url);
 
       fragment.append(newsClone);
     });
 
-    findElement<HTMLElement>(document, '.news').innerHTML = '';
-    findElement<HTMLElement>(document, '.news').appendChild(fragment);
+    queryElement(document, '.news', HTMLElement).innerHTML = '';
+    queryElement(document, '.news', HTMLElement).appendChild(fragment);
   }
 }
 
