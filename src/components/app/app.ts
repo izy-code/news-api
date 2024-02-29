ee1800673e4e863f09f6e99fddfcec53fc4d1234
Izy <126877709+izy-code@users.noreply.github.com>
2024-02-29 17:33:36 +0300

    feat: init burger at app module and use getClosestFromEventTarget func

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,4 +1,4 @@
-import { queryElement } from '@/utils';
+import { getClosestFromEventTarget, queryElement } from '@/utils';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
@@ -13,26 +13,22 @@ class App {
 
   public start(): void {
     queryElement(document, '.languages', HTMLDivElement).addEventListener('click', (e) => {
-      const target = e.target;
+      const button = getClosestFromEventTarget(e, '.languages__button');
 
-      if (!(target instanceof HTMLElement)) {
-        return;
+      if (button instanceof HTMLButtonElement) {
+        const sourcesOnLanguage = this.view
+          .getSourcesData()
+          .filter((source) => source.language === button.dataset.lang);
+
+        this.view.sources.draw(sourcesOnLanguage);
       }
-
-      const button = target.closest('.languages__button');
-
-      if (!(button instanceof HTMLButtonElement)) {
-        return;
-      }
-
-      const sourcesOnLanguage = this.view.getSourcesData().filter((source) => source.language === button.dataset.lang);
-
-      this.view.sources.draw(sourcesOnLanguage);
     });
 
     queryElement(document, '.sources', HTMLDivElement).addEventListener('click', (e) =>
       this.controller.getNews(e, (data) => this.view.drawNews(data)),
     );
+
+    this.controller.burger.initBurger();
 
     this.controller.getSources((data) => this.view.drawLanguages(data));
   }
