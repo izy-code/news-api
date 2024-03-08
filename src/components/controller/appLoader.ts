2ba641d766ccb0583a586de24997a1987d8cf079
Izy <126877709+izy-code@users.noreply.github.com>
2024-03-09 00:31:19 +0300

    fix: add API key and URL default values

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,9 +1,12 @@
 import Loader from './loader';
 
+const apiUrl = process.env.API_URL || 'https://newsapi.org/v2/';
+const apiKey = process.env.API_KEY || ''; // An empty string was used as the default API key due to the unknown default key for the API
+
 class AppLoader extends Loader {
   constructor() {
-    super(process.env.API_URL, {
-      apiKey: process.env.API_KEY,
+    super(apiUrl, {
+      apiKey: apiKey,
     });
   }
 }
